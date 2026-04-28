20dc6ba94dcc0228f748860778e99dc974fd1249
dunghoangtien0504 <dunghoangtien0504@gmail.com>
2026-04-28 11:44:57 +0700

    Delete api/test-env.js

--- api/test-env.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    smtp_pass_exists: !!process.env.SMTP_PASS,
-    smtp_pass_prefix: process.env.SMTP_PASS?.substring(0, 8) || 'MISSING',
-    sepay_merchant_exists: !!process.env.SEPAY_MERCHANT_ID,
-    sepay_secret_exists: !!process.env.SEPAY_SECRET_KEY
-  });
-}
